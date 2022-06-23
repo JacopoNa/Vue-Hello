bc50b994425c25bc59cb0ccb9eb459ddac896bc8
JacopoNa <j.nardelli@virgilio.it>
2022-06-23 15:36:00 +0200

    bonus done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
+// BONUS:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data (in data avrò l'src).
+
 var app = new Vue({
     el: '#root',
     data : {
-        message: 'Questo è un titolo'
+        message: 'Questo è San Siro',
+        image: 'https://images2.corriereobjects.it/methode_image/socialshare/2021/10/13/c4bb4730-2be9-11ec-98f9-fbd4bdd13a87.jpg'
     }
 })
